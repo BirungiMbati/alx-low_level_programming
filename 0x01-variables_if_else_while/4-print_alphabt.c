1304ac03af989b3660338315b5dcc91534e0cf61
BirungiMbati <birungimehits@gmail.com>
2023-02-22 15:13:57 -0800

    Variables_if_else_while Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabet in lowercase.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'a') {
+			if ((c != 'q' && c <= 'z')
+					putchar(c);
+				c++;
+		}
+	putchar('\n');
+	return (0);
+}
